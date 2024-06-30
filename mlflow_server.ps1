2aa1dc66ab9166d43198382ec9638dbc996a2a68
ronaldvlaar <51344990+ronaldvlaar@users.noreply.github.com>
2024-06-30 21:26:18 +0200

    Delete mlflow_server.ps1

--- mlflow_server.ps1
+++
@@ -1,6 +0,0 @@
-Write-Host "Starting MLflow server..."
-rm -r E:/home/rvlaar/mlflow
-cp -r E:/home/rvlaar/.mlflow/ E:/home/rvlaar/mlflow 
-python update_meta.py 1
-mlflow server `
-    --backend-store-uri file://E:/home/rvlaar/mlflow
